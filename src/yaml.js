331eb47c564357bb9b24d8788f9b9b00c2d6d36e
Francisco López <donflopez@gmail.com>
2016-11-24 17:08:44 +0100

    Update default conf

--- src/yaml.js
+++ src/yaml.js
@@ -14,32 +14,27 @@ const getBaseProperties = function getBaseProperties() {
     global: {
       language: 'en'
     },
-
-    cameras: {
-      prespective: {
-        type: 'perspective',
-        vanishing_point: [ '0', '-250px' ],
-        active: false
-      },
-
-      isometric: {
-        type: 'isometric',
-        axis: [ 0, 1 ],
-        active: true
-      },
-      flat: {
-        type: 'flat',
-        active: false
-      }
-    },
-
     fonts: {
       Montserrat: {
         url: 'https://fonts.gstatic.com/s/montserrat/v7/zhcz-_WihjSQC0oHJ9TCYL3hpw3pgy2gAi-Ip7WPMi0.woff'
       }
     },
 
-    layers: {}
+    layers: {},
+    styles: {
+        polygons_blend: {
+            base: 'polygons',
+            blend: 'depthinlay'
+        },
+        lines_blend: {
+            base: 'lines',
+            blend: 'depthinlay'
+        },
+        points_blend: {
+            base: 'points',
+            blend: 'depthinlay'
+        }
+    }
   };
 };
 
