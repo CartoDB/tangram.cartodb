0e90bc8f21f845413bec37306e929945ba6cbbb9
Raul Ochoa <rochoaf@gmail.com>
2017-06-27 14:28:34 +0200

    Avoid creating a global var

--- src/tangram.js
+++ src/tangram.js
@@ -51,7 +51,7 @@ TC.prototype = {
 
   getTotalGeometries: function() {
     let sum = 0;
-    for ( tile in this.scene.tile_manager.tiles ) {
+    for (var tile in this.scene.tile_manager.tiles ) {
       sum += this.scene.tile_manager.tiles[tile].debug.geometry_count;
     }
 
